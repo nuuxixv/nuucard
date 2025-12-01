c0fbff2b4c2899531b04f16a131a84019cec293f
Your <your_email@example.com>
2025-12-02 00:06:11 +0900

    kakao 메시지 공유 기능 추가

--- src/components/KakaoScript.tsx
+++ src/components/KakaoScript.tsx
@@ -26,7 +26,7 @@ export default function KakaoScript() {
     return (
         <Script
             src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js"
-            integrity="sha384-TiCUE00h649CAMonG018J2ujOgDKW/kVWlChEuu4jK2txfDWp1VidGi78b27breR"
+            integrity="sha384-TiCUE00h649CAMonG018J2ujOgDKW/kVWlChEuu4jK2vxfAAD0eZxzCKakxg55G4"
             crossOrigin="anonymous"
             onLoad={onLoad}
         />
